7a0b73b4ee37163f1785b395cbf41bc4460f4880
Thomas Johnson <thomas.johnson@alpegagroup.com>
2024-03-07 09:25:12 +0100

    Fixes logic to receive data in batches from BigQuery

--- src/bq_row_iterator.py
+++ src/bq_row_iterator.py
@@ -1,16 +1,23 @@
 from google.cloud.bigquery_storage import BigQueryReadClient
+from multicorn.utils import log_to_postgres, ERROR, WARNING, INFO, DEBUG
 
 
 class BigQueryRowIterator(object):
-    def next_batch(self):
-        self.current_batch = next(self.arrow_iterable).to_pylist()
-
-    def __init__(self, query_result, max_queue_size=2, bqstorage_client=None):
+    def __init__(self, query_result, max_queue_size=10, bqstorage_client=None):
+        self.rows_processed = 0
+        self.total_rows = query_result.total_rows
         self.arrow_iterable = query_result.to_arrow_iterable(
             max_queue_size=max_queue_size,
             bqstorage_client=bqstorage_client or BigQueryReadClient(),
         )
         self.next_batch()
+
+    def next_batch(self):
+        log_to_postgres(
+            f"Percent of rows processed: {self.rows_processed / self.total_rows:.2%}.",
+            DEBUG,
+        )
+        self.current_batch = next(self.arrow_iterable).to_pylist()
 
     def __iter__(self):
         return self
@@ -20,6 +27,7 @@ class BigQueryRowIterator(object):
 
     def next(self):
         if self.current_batch:
+            self.rows_processed = self.rows_processed + 1
             return self.current_batch.pop(0)
         else:
             self.next_batch()
